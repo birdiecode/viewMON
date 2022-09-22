c77aca3c69ad1873fe2eab2bad308d411fb5d809
birdiecode <birdiecode@gmail.com>
2022-09-22 09:37:59 +0300

    добавлен парсинг конфигурационного файла

--- main.py
+++ main.py
@@ -13,8 +13,26 @@ async def echo(websocket):
                     with open('/var/run/viewmon/'+dev) as devinfo:
                         await websocket.send(json.dumps({"method": "set_data", "data": {"dev": dev, "info": devinfo.read()}}))
                 except Exception:
-                    await websocket.send(json.dumps({"method": "set_data", "data": {"dev": dev, "info": "information about the "+dev+" device not found"}}))
+                    await websocket.send(json.dumps(
+                        {"method": "set_data", "data": {"dev": dev, "info": "information about the "+dev+" device not found"}}))
                     print("no dev: "+dev)
+        elif data["method"] == "get_settings":
+            try:
+                with open('/etc/viewmon/grid.cfg') as gridinfo:
+                    grid = {"status": "ok", "grid": []}
+                    for line in gridinfo:
+                        line = line.replace("\n", "")
+                        if line[0] == "#":
+                            continue
+
+                        sline = line.split(" ")
+                        grid['grid'].append([int(sline[0]), int(sline[1]), int(sline[2]), int(sline[3]), int(sline[4]), int(sline[5]), sline[6]])
+                    await websocket.send(json.dumps(
+                        {"method": "set_grid", "data": grid}))
+            except Exception:
+                await websocket.send(json.dumps(
+                    {"method": "set_grid", "data": {"status": "error", "info": "configuration error"}}))
+
 
 
 async def main():
